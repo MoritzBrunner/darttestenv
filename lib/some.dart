5d20b66766cbb5a496dc58b2bda8616136b37b05
Moritz Brunner <Brunner.Mori@gmail.com>
2021-11-02 11:02:51 +0100

    backup

--- lib/some.dart
+++ lib/some.dart
@@ -39,13 +39,13 @@
 /// - create anchor file, anchor link to bundle form index-1 in list
 /// - move all pages form the old folder to the new
 ///   get address index+1 put in list, if index+2 is page (and not spacer) put address in list, if index+x is spacer break
-///   each address in list rename to folder and page_1 2 3 > problem because some systems 123 other 321 
+///   each address in list rename to folder and page_1 2 3 > problem because some systems 123 other 321 > page_unique key (later make a clean name function)
 /// - done
 /// 
 /// delete a spacer
 /// var index = where spacer got removed
 /// File side:
-/// - move all pages to the other folder
+/// - move all pages to the upper folder
 ///   get address index put in list, if index+1 is page (and not spacer) put address in list, if index+x is spacer break
 ///   each address in list rename to folder from index-1 and page_j+1 j+2 j+3 (j = page name index-1) and link each anchor mass linking not possible with anchors because DTN, sort by page name not possible because some systems sort 123 other 321, it is possible with sleep function but than maybe microseconds?
 /// 
@@ -58,3 +58,31 @@
 /// - use anchor with milliseconds but mass linking becomes slow fast
 /// - use number, needs renaming of all other (no just the same ones that also need anchor change, but with creation in between a lot need renaming), not safe when individual pages get removed
 /// - use register together with unique keys, not safe when individual pages get removed, complete order is lost when register is lost
+/// 
+/// 
+/// 
+/// 
+/// 
+/// /// create a spacer
+/// var index = where spacer has been inserted
+/// File side:
+/// - create new folder, name form bundle of index-1 in list
+/// - create anchor file, anchor link to bundle form index-1 in list
+/// - move all pages form the old folder to the new
+///   get address index+1 put in list, if index+2 is page (and not spacer) put address in list, if index+x is spacer break
+///   each address in list rename to folder and page_unique key (later make a clean name function)
+/// - done
+/// 
+/// 
+/// /// delete a spacer
+/// var index = where spacer got removed
+/// File side:
+/// - move all pages to the upper folder
+///   get address index put in list, if index+1 is page (and not spacer) put address in list, if index+x is spacer break
+///   each address in list {rename to folder from index-1 and page_unique key, link each anchor to the anchor index-n} (later make a clean name function)
+/// - delete folder from index
+/// - done
+/// 
+/// /// move a spacer
+/// File side:
+/// - a combination of create and delete
